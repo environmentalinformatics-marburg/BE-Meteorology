920e1ad5ec2d8ec63a3a735b5f1794bb721fe860
swoellauer <swoellauer>
2019-02-13 10:08:57 +0100

    residuals prediction with climate data

--- src/model_by_lui.R
+++ src/model_by_lui.R
@@ -19,12 +19,14 @@ year[year == "separately(2014)"] <- "2014"
 year[year == "separately(2015)"] <- "2015"
 year[year == "separately(2016)"] <- "2016"
 lui_df = data.frame(EpPlotID = lui_csv$EP.Plotid, Year = year, lui_g = lui_csv$G_std, lui_m = lui_csv$M_std, lui_f = lui_csv$F_std, lui = lui_csv$LUI)
+lui_df = lui_df[complete.cases(lui_df), ]
 write.csv(lui_df, paste0(path_output, "lui.csv"))
 
 #prepare climate
 climate_csv = read.csv(paste0(path_input, "plots.csv"))
 plotid = substring(climate_csv$plotID, 1, 5)
-climate_df = data.frame(Useful_EP_PlotID = plotid, Year=climate_csv$datetime, Ta_200 = climate_csv$Ta_200, Ta_200_DTR = climate_csv$Ta_200_DTR)
+climate_df = data.frame(Useful_EP_PlotID = plotid, Year=climate_csv$datetime, Ta_200 = climate_csv$Ta_200, Ta_200_DTR = climate_csv$Ta_200_DTR, a = climate_csv$Ta_200^1.1, b = climate_csv$Ta_200_DTR^1.5)
+climate_df = climate_df[complete.cases(climate_df), ]
 write.csv(climate_df, paste0(path_output, "climate.csv"))
 
 #merge data.frames
@@ -38,7 +40,7 @@ pred_lui_indices <- c("lui_g", "lui_m", "lui_f", "lui")
 print(names(org_df[,pred_lui_indices]))
 
 #climate predictors
-pred_climate_indices <- c("Ta_200", "Ta_200_DTR")
+pred_climate_indices <- c("Ta_200", "Ta_200_DTR", "a", "b")
 print(names(org_df[,pred_climate_indices]))
 
 
@@ -77,7 +79,7 @@ for(rv in rspvars){
                           #repeats = 1, 
                           verbose = FALSE)
     
-    model = ffs(predictors = act_df[, pred_lui_indices], 
+    model_lui = ffs(predictors = act_df[, pred_lui_indices], 
                 response = act_df[, rv],  
                 metric = "RMSE", 
                 method = "rf",
@@ -85,15 +87,38 @@ for(rv in rspvars){
                 #tuneLength = tune_length,
                 #tuneGrid = lut$MTHD_DEF_LST[["rf"]]$tunegr
                 importance = TRUE)
-    saveRDS(model, file = paste0(path_output, "model_", rv, "_", icv, ".rds"))
+    saveRDS(model_lui, file = paste0(path_output, "model_lui_", rv, "_", icv, ".rds"))
+    print(model_lui)
     
     test_df = df[indp_cv$indexOut[[icv]],]
-    m_prediction = predict(model, test_df)
-    m_res = test_df[, rv] - m_prediction
+    m_lui_prediction = predict(model_lui, test_df)
+    m_lui_res = test_df[, rv] - m_lui_prediction
     
-    m_df = data.frame(m_prediction = m_prediction, m_res = m_res)
-    saveRDS(m_df, file = paste0(path_output, "m_", rv, "_", icv, ".rds"))
-    write.csv(m_df, file = paste0(path_output, "m_", rv, "_", icv, ".csv"))
+    m_lui_df = data.frame(m_lui_prediction = m_lui_prediction, m_lui_res = m_lui_res)
+    saveRDS(m_lui_df, file = paste0(path_output, "m_lui_", rv, "_", icv, ".rds"))
+    write.csv(m_lui_df, file = paste0(path_output, "m_lui_", rv, "_", icv, ".csv"))
+    
+    #prepare response of residuals in same order as in act_df
+    m_climate_response = act_df[, rv] - predict(model_lui, act_df)
+    print(m_climate_response)
+    
+    model_climate = ffs(predictors = act_df[, pred_climate_indices], 
+                        response = m_climate_response,  
+                        metric = "RMSE", 
+                        method = "rf",
+                        trControl = trCntr,
+                        #tuneLength = tune_length,
+                        #tuneGrid = lut$MTHD_DEF_LST[["rf"]]$tunegr
+                        importance = TRUE)
+    saveRDS(model_climate, file = paste0(path_output, "model_climate_", rv, "_", icv, ".rds"))
+    print(model_climate)
+    
+    m_climate_prediction = predict(model_climate, test_df)
+    m_climate_res = m_lui_res - m_climate_prediction
+    
+    m_climate_df = data.frame(m_lui_prediction = m_lui_prediction, m_lui_res = m_lui_res, m_climate_prediction = m_climate_prediction, m_climate_res = m_climate_res)
+    saveRDS(m_climate_df, file = paste0(path_output, "m_climate_", rv, "_", icv, ".rds"))
+    write.csv(m_climate_df, file = paste0(path_output, "m_climate_", rv, "_", icv, ".csv"))    
   }
 }
 
