8efbcb65fe9c2708c766f4b8080f292cf07fe2f7
swoellauer <swoellauer>
2019-02-14 15:49:56 +0100

    precipitation, Ta_200_growing_degree_days_10

--- src/model_by_lui.R
+++ src/model_by_lui.R
@@ -1,3 +1,6 @@
+#!/usr/bin/Rscript
+#note: needs to be run from parent diretory. e.g. ./src/model_by_lui.R
+
 source("src/config.R")
 
 library(CAST)
@@ -22,11 +25,21 @@ lui_df = data.frame(EpPlotID = lui_csv$EP.Plotid, Year = year, lui_g = lui_csv$G
 lui_df = lui_df[complete.cases(lui_df), ]
 write.csv(lui_df, paste0(path_output, "lui.csv"))
 
+#prepare temperature
+temperature_csv = read.csv(paste0(path_input, "temperature.csv"))
+plotid = substring(temperature_csv$plotID, 1, 5)
+temperature_df = data.frame(Useful_EP_PlotID = plotid, Year=temperature_csv$datetime, Ta_200 = temperature_csv$Ta_200, Ta_200_DTR = temperature_csv$Ta_200_DTR, Ta_200_growing_degree_days_10 = temperature_csv$Ta_200_growing_degree_days_10)
+temperature_df = temperature_df[complete.cases(temperature_df), ]
+write.csv(temperature_df, paste0(path_output, "temperature.csv"))
+
+#prepare precipitation
+precipitation_csv = read.csv(paste0(path_input, "precipitation_radolan.csv"))
+precipitation_df = data.frame(Useful_EP_PlotID = precipitation_csv$plotID, Year=precipitation_csv$datetime, precipitation_radolan = precipitation_csv$precipitation_radolan, precipitation_radolan_rain_days = precipitation_csv$precipitation_radolan_rain_days)
+precipitation_df = precipitation_df[complete.cases(precipitation_df), ]
+write.csv(precipitation_df, paste0(path_output, "precipitation.csv"))
+
 #prepare climate
-climate_csv = read.csv(paste0(path_input, "plots.csv"))
-plotid = substring(climate_csv$plotID, 1, 5)
-climate_df = data.frame(Useful_EP_PlotID = plotid, Year=climate_csv$datetime, Ta_200 = climate_csv$Ta_200, Ta_200_DTR = climate_csv$Ta_200_DTR, a = climate_csv$Ta_200^1.1, b = climate_csv$Ta_200_DTR^1.5)
-climate_df = climate_df[complete.cases(climate_df), ]
+climate_df <- merge(temperature_df, precipitation_df, by=c("Useful_EP_PlotID", "Year"))
 write.csv(climate_df, paste0(path_output, "climate.csv"))
 
 #merge data.frames
@@ -40,17 +53,20 @@ pred_lui_indices <- c("lui_g", "lui_m", "lui_f", "lui")
 print(names(org_df[,pred_lui_indices]))
 
 #climate predictors
-pred_climate_indices <- c("Ta_200", "Ta_200_DTR", "a", "b")
+pred_climate_indices <- c("Ta_200", "Ta_200_DTR", "Ta_200_growing_degree_days_10", "precipitation_radolan", "precipitation_radolan_rain_days")
 print(names(org_df[,pred_climate_indices]))
 
 
 # variables (number.herbs.with.shrubs to shannon)
-rspvars = c("number.herbs.with.shrubs", "number.grasses.with.shrubs")
+rspvars = c("number.herbs.with.shrubs", "number.grasses.with.shrubs") # TODO
 # ! FOR TESTING !
-rspvars = c("shannon")
+rspvars = c("biomass_g") # ! FOR TESTING !
 
-k_fold = 2  # 10
-kt_fold = 2 # ? (k_fold - 1)
+k_fold = 10
+kt_fold = 9
+# ! FOR TESTING !
+#k_fold = 2   # ! FOR TESTING !
+#kt_fold = 2  # ! FOR TESTING !
 
 # Loop over all variables (number.herbs to shannon)
 for(rv in rspvars){
@@ -60,7 +76,7 @@ for(rv in rspvars){
   df <- org_df[!is.na(org_df[rv]),]  
   
   # k-fold Leave-Location-Out cross validation (over non NA data)
-  indp_cv = CreateSpacetimeFolds(df, spacevar = "Useful_EP_PlotID", timevar = NA, k = k_fold, seed = 31051974)
+  indp_cv = CAST::CreateSpacetimeFolds(df, spacevar = "Useful_EP_PlotID", timevar = NA, k = k_fold, seed = 31051974)
   
   # cross validation loop
   for(icv in seq(length(indp_cv$index))){
@@ -69,24 +85,24 @@ for(rv in rspvars){
     act_df = df[-indp_cv$indexOut[[icv]],]
     
     # kt-fold Leave-Location-Out cross validation for training
-    cv_indicies = CreateSpacetimeFolds(act_df, spacevar = "Useful_EP_PlotID", timevar = NA, k = kt_fold, seed = 65451994)
+    cv_indicies = CAST::CreateSpacetimeFolds(act_df, spacevar = "Useful_EP_PlotID", timevar = NA, k = kt_fold, seed = 65451994)
     
     # Set train control
-    trCntr = trainControl(method="cv",
-                          index = cv_indicies$index, 
-                          indexOut = cv_indicies$indexOut,
-                          returnResamp = "all",
-                          #repeats = 1, 
-                          verbose = FALSE)
+    trCntr = caret::trainControl(method="cv",
+                                index = cv_indicies$index, 
+                                indexOut = cv_indicies$indexOut,
+                                returnResamp = "all",
+                                #repeats = 1, 
+                                verbose = FALSE)
     
-    model_lui = ffs(predictors = act_df[, pred_lui_indices], 
-                response = act_df[, rv],  
-                metric = "RMSE", 
-                method = "rf",
-                trControl = trCntr,
-                #tuneLength = tune_length,
-                #tuneGrid = lut$MTHD_DEF_LST[["rf"]]$tunegr
-                importance = TRUE)
+    model_lui = CAST::ffs(predictors = act_df[, pred_lui_indices], 
+                          response = act_df[, rv],  
+                          metric = "RMSE", 
+                          method = "rf",
+                          trControl = trCntr,
+                          #tuneLength = tune_length,
+                          #tuneGrid = lut$MTHD_DEF_LST[["rf"]]$tunegr
+                          importance = TRUE)
     saveRDS(model_lui, file = paste0(path_output, "model_lui_", rv, "_", icv, ".rds"))
     print(model_lui)
     
@@ -94,29 +110,29 @@ for(rv in rspvars){
     m_lui_prediction = predict(model_lui, test_df)
     m_lui_res = test_df[, rv] - m_lui_prediction
     
-    m_lui_df = data.frame(m_lui_prediction = m_lui_prediction, m_lui_res = m_lui_res)
+    m_lui_df = data.frame(response = test_df[rv], m_lui_prediction = m_lui_prediction, m_lui_res = m_lui_res)
     saveRDS(m_lui_df, file = paste0(path_output, "m_lui_", rv, "_", icv, ".rds"))
     write.csv(m_lui_df, file = paste0(path_output, "m_lui_", rv, "_", icv, ".csv"))
     
     #prepare response of residuals in same order as in act_df
     m_climate_response = act_df[, rv] - predict(model_lui, act_df)
-    print(m_climate_response)
+    #print(m_climate_response)
     
-    model_climate = ffs(predictors = act_df[, pred_climate_indices], 
-                        response = m_climate_response,  
-                        metric = "RMSE", 
-                        method = "rf",
-                        trControl = trCntr,
-                        #tuneLength = tune_length,
-                        #tuneGrid = lut$MTHD_DEF_LST[["rf"]]$tunegr
-                        importance = TRUE)
+    model_climate = CAST::ffs(predictors = act_df[, pred_climate_indices], 
+                          response = m_climate_response,  
+                          metric = "RMSE", 
+                          method = "rf",
+                          trControl = trCntr,
+                          #tuneLength = tune_length,
+                          #tuneGrid = lut$MTHD_DEF_LST[["rf"]]$tunegr
+                          importance = TRUE)
     saveRDS(model_climate, file = paste0(path_output, "model_climate_", rv, "_", icv, ".rds"))
     print(model_climate)
     
     m_climate_prediction = predict(model_climate, test_df)
     m_climate_res = m_lui_res - m_climate_prediction
     
-    m_climate_df = data.frame(m_lui_prediction = m_lui_prediction, m_lui_res = m_lui_res, m_climate_prediction = m_climate_prediction, m_climate_res = m_climate_res)
+    m_climate_df = data.frame(response = test_df[rv], m_lui_prediction = m_lui_prediction, m_lui_res = m_lui_res, m_climate_prediction = m_climate_prediction, m_climate_res = m_climate_res)
     saveRDS(m_climate_df, file = paste0(path_output, "m_climate_", rv, "_", icv, ".rds"))
     write.csv(m_climate_df, file = paste0(path_output, "m_climate_", rv, "_", icv, ".csv"))    
   }
