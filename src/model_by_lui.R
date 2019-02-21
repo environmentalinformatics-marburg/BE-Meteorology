becabe3ee5445bd708712d8438bac2ac44f33a20
swoellauer <swoellauer>
2019-02-21 09:08:55 +0100

    filter by growth months; vis climate

--- src/model_by_lui.R
+++ src/model_by_lui.R
@@ -1,15 +1,15 @@
 #!/usr/bin/Rscript
-#note: needs to be run from parent directory. e.g. ./src/model_by_lui.R
+# note: needs to be run from parent directory. e.g. ./src/model_by_lui.R
 
 source("src/config.R")
 
 library(CAST)
 library(caret)
 
-#prepare vegetation
+# prepare vegetation
 vegetation_df <- read.csv(paste0(path_input, "Vegetation_HeaderData_2008-2016.csv"))
 
-#prepare LUI
+# prepare LUI
 lui_csv = read.csv(paste0(path_input, "LUI_glob_sep_11.02.2019+185450.txt"), sep = "\t", dec=',')
 year <- as.character(lui_csv$Std_procedure.year.)
 year[year == "separately(2008)"] <- "2008"
@@ -24,38 +24,63 @@ year[year == "separately(2016)"] <- "2016"
 lui_df = data.frame(EpPlotID = lui_csv$EP.Plotid, Year = year, lui_g = lui_csv$G_std, lui_m = lui_csv$M_std, lui_f = lui_csv$F_std, lui = lui_csv$LUI)
 lui_df = lui_df[complete.cases(lui_df), ]
 
-#!! filter by year !!
+# !! filter by year !!
 lui_df = lui_df[lui_df$Year == 2015, ]
 
 write.csv(lui_df, paste0(path_output, "lui.csv"))
 
 
-#prepare temperature
-temperature_csv = read.csv(paste0(path_input, "temperature.csv"))
-plotid = substring(temperature_csv$plotID, 1, 5)
-temperature_df = data.frame(Useful_EP_PlotID = plotid, Year=temperature_csv$datetime, Ta_200 = temperature_csv$Ta_200, Ta_200_DTR = temperature_csv$Ta_200_DTR, Ta_200_growing_degree_days_10 = temperature_csv$Ta_200_growing_degree_days_10)
+# prepare temperature full year
+#temperature_csv = read.csv(paste0(path_input, "temperature.csv"))
+#plotid = substring(temperature_csv$plotID, 1, 5)
+#temperature_df = data.frame(Useful_EP_PlotID = plotid, Year=temperature_csv$datetime, Ta_200 = temperature_csv$Ta_200, Ta_200_DTR = temperature_csv$Ta_200_DTR, Ta_200_growing_degree_days_10 = temperature_csv$Ta_200_growing_degree_days_10)
+#temperature_df = temperature_df[complete.cases(temperature_df), ]
+#write.csv(temperature_df, paste0(path_output, "temperature.csv"))
+
+# prepare temperature growth months
+temperature_month_csv = read.csv(paste0(path_input, "temperature_month.csv"))
+temperature_month_csv$plot = substring(temperature_month_csv$plotID, 1, 5)
+temperature_month_csv$year = as.numeric(substring(temperature_month_csv$datetime, 1, 4))
+temperature_month_csv$month = as.numeric(substring(temperature_month_csv$datetime, 6, 7))
+temperature_month_csv$growth =  temperature_month_csv$month >= 1 & temperature_month_csv$month <= 6
+temperature_month_csv = temperature_month_csv[temperature_month_csv$growth,]
+temperature_df = data.frame(Useful_EP_PlotID = temperature_month_csv$plot, Year=temperature_month_csv$year, Ta_200 = temperature_month_csv$Ta_200, Ta_200_DTR = temperature_month_csv$Ta_200_DTR, Ta_200_growing_degree_days_10 = temperature_month_csv$Ta_200_growing_degree_days_10)
 temperature_df = temperature_df[complete.cases(temperature_df), ]
+temperature_df <- aggregate(temperature_df[,c("Ta_200", "Ta_200_DTR", "Ta_200_growing_degree_days_10")], by = list(Useful_EP_PlotID = temperature_df$Useful_EP_PlotID, Year = temperature_df$Year), FUN = mean, na.rm = TRUE)
 write.csv(temperature_df, paste0(path_output, "temperature.csv"))
 
-#prepare precipitation
-precipitation_csv = read.csv(paste0(path_input, "precipitation_radolan.csv"))
-precipitation_df = data.frame(Useful_EP_PlotID = precipitation_csv$plotID, Year=precipitation_csv$datetime, precipitation_radolan = precipitation_csv$precipitation_radolan, precipitation_radolan_rain_days = precipitation_csv$precipitation_radolan_rain_days)
+
+# prepare precipitation full year
+#precipitation_csv = read.csv(paste0(path_input, "precipitation_radolan.csv"))
+#precipitation_df = data.frame(Useful_EP_PlotID = precipitation_csv$plotID, Year=precipitation_csv$datetime, precipitation_radolan = precipitation_csv$precipitation_radolan, precipitation_radolan_rain_days = precipitation_csv$precipitation_radolan_rain_days)
+#precipitation_df = precipitation_df[complete.cases(precipitation_df), ]
+#write.csv(precipitation_df, paste0(path_output, "precipitation.csv"))
+
+# prepare precipitation growth months
+precipitation_month_csv = read.csv(paste0(path_input, "precipitation_radolan_month.csv"))
+precipitation_month_csv$year = as.numeric(substring(precipitation_month_csv$datetime, 1, 4))
+precipitation_month_csv$month = as.numeric(substring(precipitation_month_csv$datetime, 6, 7))
+precipitation_month_csv$growth =  precipitation_month_csv$month >= 1 & precipitation_month_csv$month <= 6
+precipitation_month_csv = precipitation_month_csv[precipitation_month_csv$growth,]
+precipitation_df = data.frame(Useful_EP_PlotID = precipitation_month_csv$plotID, Year=precipitation_month_csv$year, precipitation_radolan = precipitation_month_csv$precipitation_radolan, precipitation_radolan_rain_days = precipitation_month_csv$precipitation_radolan_rain_days)
 precipitation_df = precipitation_df[complete.cases(precipitation_df), ]
+precipitation_df <- aggregate(precipitation_df[,c("precipitation_radolan", "precipitation_radolan_rain_days")], by = list(Useful_EP_PlotID = precipitation_df$Useful_EP_PlotID, Year = precipitation_df$Year), FUN = sum, na.rm = TRUE)
 write.csv(precipitation_df, paste0(path_output, "precipitation.csv"))
 
-#prepare climate
+
+# prepare climate
 climate_df <- merge(temperature_df, precipitation_df, by=c("Useful_EP_PlotID", "Year"))
 write.csv(climate_df, paste0(path_output, "climate.csv"))
 
-#merge data.frames
+# merge data.frames
 org_df <- merge(vegetation_df, lui_df, by=c("EpPlotID", "Year"))
 org_df <- merge(org_df, climate_df, by=c("Useful_EP_PlotID", "Year"))
 write.csv(org_df, paste0(path_output, "vegetation_lui_climate.csv"))
 #str(org_df) # types of columns
 
-pdf(paste0(path_output, "plots.pdf"), width=20, height=20)
+pdf(paste0(path_output, "plots.pdf"), width=20, height=40)
 pch =  "."
-par(mfrow=c(4,4))
+par(mfrow=c(8,4))
 
 #par(mfrow=c(2,2))
 plot(org_df$biomass_g ~ org_df$lui_g, pch = pch)
@@ -64,10 +89,10 @@ plot(org_df$biomass_g ~ org_df$lui_f, pch = pch)
 plot(org_df$biomass_g ~ org_df$lui, pch = pch)
 
 #par(mfrow=c(2,2))
-plot(org_df$doy_biomass ~ org_df$lui_g, pch = pch)
-plot(org_df$doy_biomass ~ org_df$lui_m, pch = pch)
-plot(org_df$doy_biomass ~ org_df$lui_f, pch = pch)
-plot(org_df$doy_biomass ~ org_df$lui, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$lui_g, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$lui_m, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$lui_f, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$lui, pch = pch)
 
 #par(mfrow=c(2,2))
 plot(org_df$shannon ~ org_df$lui_g, pch = pch)
@@ -81,13 +106,37 @@ plot(org_df$number.herbs.with.shrubs ~ org_df$lui_m, pch = pch)
 plot(org_df$number.herbs.with.shrubs ~ org_df$lui_f, pch = pch)
 plot(org_df$number.herbs.with.shrubs ~ org_df$lui, pch = pch)
 
+#par(mfrow=c(2,2))
+plot(org_df$biomass_g ~ org_df$Ta_200, pch = pch)
+plot(org_df$biomass_g ~ org_df$Ta_200_growing_degree_days_10, pch = pch)
+plot(org_df$biomass_g ~ org_df$precipitation_radolan, pch = pch)
+plot(org_df$biomass_g ~ org_df$precipitation_radolan_rain_days, pch = pch)
+
+#par(mfrow=c(2,2))
+plot(org_df$total.cover.cum ~ org_df$Ta_200, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$Ta_200_growing_degree_days_10, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$precipitation_radolan, pch = pch)
+plot(org_df$total.cover.cum ~ org_df$precipitation_radolan_rain_days, pch = pch)
+
+#par(mfrow=c(2,2))
+plot(org_df$shannon ~ org_df$Ta_200, pch = pch)
+plot(org_df$shannon ~ org_df$Ta_200_growing_degree_days_10, pch = pch)
+plot(org_df$shannon ~ org_df$precipitation_radolan, pch = pch)
+plot(org_df$shannon ~ org_df$precipitation_radolan_rain_days, pch = pch)
+
+#par(mfrow=c(2,2))
+plot(org_df$number.herbs.with.shrubs ~ org_df$Ta_200, pch = pch)
+plot(org_df$number.herbs.with.shrubs ~ org_df$Ta_200_growing_degree_days_10, pch = pch)
+plot(org_df$number.herbs.with.shrubs ~ org_df$precipitation_radolan, pch = pch)
+plot(org_df$number.herbs.with.shrubs ~ org_df$precipitation_radolan_rain_days, pch = pch)
+
 dev.off()
 
 # LUI predictors
 pred_lui_indices <- c("lui_g", "lui_m", "lui_f", "lui")
 print(names(org_df[,pred_lui_indices]))
 
-#climate predictors
+# climate predictors
 pred_climate_indices <- c("Ta_200", "Ta_200_DTR", "Ta_200_growing_degree_days_10", "precipitation_radolan", "precipitation_radolan_rain_days")
 print(names(org_df[,pred_climate_indices]))
 
